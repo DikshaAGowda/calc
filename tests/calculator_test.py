6d41046140361a950b26e5609ecc7b66fb97a0d2
DikshaAGowda <dg63@njit.edu>
2021-12-04 15:04:54 -0800

    Updated the exception code.

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,26 +1,30 @@
 """Testing the Calculator"""
 import pytest
 
-from calculator.main import Calculator
+from calc.calculator.main import Calculator
+
 
 def test_calculator_result():
     """testing calculator result is 0"""
     calc = Calculator()
     assert calc.result == 0
 
+
 def test_calculator_add():
     """Testing the Add function of the calculator"""
-    #Arrange by instantiating the calc class
+    # Arrange by instantiating the calc class
     calc = Calculator()
-    #Act by calling the method to be tested
+    # Act by calling the method to be tested
     calc.add_number(4)
-    #Assert that the results are correct
+    # Assert that the results are correct
     assert calc.result == 4
+
 
 def test_calculator_get_result():
     """Testing the Get result method of the calculator"""
     calc = Calculator()
     assert calc.get_result() == 0
+
 
 def test_calculator_subtract():
     """Testing the subtract method of the calculator"""
@@ -28,11 +32,13 @@ def test_calculator_subtract():
     calc.subtract_number(1)
     assert calc.get_result() == -1
 
+
 def test_calculator_multiply():
     """ tests multiplication of two numbers"""
     calc = Calculator()
-    result  = calc.multiply_numbers(1,2)
+    result = calc.multiply_numbers(1, 2)
     assert result == 2
+
 
 def test_calculator_division():
     """ Testing division of two numbers"""
@@ -43,6 +49,7 @@ def test_calculator_division():
     result = Calculator.divide_numbers(value_a, value_b)
     # Assert
     assert result == 1
+
 
 def test_calculator_division_exception():
     """ Testing division exception for division by zero"""
